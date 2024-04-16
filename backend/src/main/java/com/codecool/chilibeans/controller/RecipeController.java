1da1601cdd926b26c6e00a76adfa3b7632f9f998
Blanka Pasztor <139960849+blankapsz@users.noreply.github.com>
2024-04-16 14:17:14 +0200

    create Get endpoints

--- backend/src/main/java/com/codecool/chilibeans/controller/RecipeController.java
+++ backend/src/main/java/com/codecool/chilibeans/controller/RecipeController.java
@@ -1,7 +1,32 @@
 package com.codecool.chilibeans.controller;
 
+import com.codecool.chilibeans.controller.dto.recipe.RecipeDTO;
+import com.codecool.chilibeans.service.ChiliService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
 
 @RestController
 public class RecipeController {
+
+    private ChiliService chiliService;
+
+    @Autowired
+    public RecipeController(ChiliService chiliService) {
+        this.chiliService = chiliService;
+    }
+
+    @GetMapping("/recipe")
+    public ResponseEntity<Set<RecipeDTO>> getAllRecipe(){
+        return ResponseEntity.ok().body(chiliService.getAllRecipe());
+    }
+
+    @GetMapping("/recipe/{id}")
+    public RecipeDTO getRecipeById(@PathVariable long id){
+        return chiliService.getRecipeById(id);
+    }
 }
