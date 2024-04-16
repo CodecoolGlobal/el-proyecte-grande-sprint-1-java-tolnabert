63e97096985520f7210510bddc9d0c5c93158696
Tumpeti <118043221+Tumpeti@users.noreply.github.com>
2024-04-16 14:59:02 +0200

    UserController created with CRUD operations

--- backend/src/main/java/com/codecool/chilibeans/controller/UserController.java
+++ backend/src/main/java/com/codecool/chilibeans/controller/UserController.java
@@ -1,7 +1,45 @@
 package com.codecool.chilibeans.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.chilibeans.controller.dto.user.NewUserDTO;
+import com.codecool.chilibeans.controller.dto.user.UserDTO;
+import com.codecool.chilibeans.model.User;
+import com.codecool.chilibeans.service.ChiliServiceInterface;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.UUID;
 
 @RestController
 public class UserController {
+
+    private final ChiliServiceInterface service;
+
+    public UserController(ChiliServiceInterface service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public Set<UserDTO> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public UserDTO getAllUserById(@PathVariable("id")UUID uuid) {
+        return service.getUserById(uuid);
+    }
+
+    @PostMapping("/users")
+    public UserDTO createNewUser(@RequestBody NewUserDTO newUserDTO) {
+        return service.createNewUser(newUserDTO);
+    }
+
+    @PatchMapping("/users/{id}")
+    public UserDTO updateUser(@PathVariable("id") UUID uuid, @RequestBody UserDTO userDTO){
+        return service.updateUser(uuid, userDTO);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public boolean deleteUser(@PathVariable("id") UUID uuid){
+        return service.DeleteUserById(uuid);
+    }
 }
