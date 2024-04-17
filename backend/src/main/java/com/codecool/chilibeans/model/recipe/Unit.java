e3dd818a0fa80b01e2ba24c3dc03658cae91c7f6
Tumpeti <118043221+Tumpeti@users.noreply.github.com>
2024-04-17 13:50:53 +0200

    Unit endpoint for CRUD operations with service and rudimentary repository functions

--- backend/src/main/java/com/codecool/chilibeans/model/recipe/Unit.java
+++ backend/src/main/java/com/codecool/chilibeans/model/recipe/Unit.java
@@ -1,13 +1,6 @@
 package com.codecool.chilibeans.model.recipe;
 
-public enum Unit {
-    ML,
-    DL,
-    L,
-    G,
-    DKG,
-    KG,
-    PC,
-    TEASPOON,
-    TABLESPOON;
+import java.util.UUID;
+
+public record Unit (long databaseId, UUID unitId, String unitName) {
 }
