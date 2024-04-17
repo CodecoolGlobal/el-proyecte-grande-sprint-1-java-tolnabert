5b554e41aee42acf2fcc84f5507644a12542d575
tolnabert <tolnabert@gmail.com>
2024-04-17 15:49:45 +0200

    add recipes page and proxy

--- frontend/src/pages/Recipes.tsx
+++ frontend/src/pages/Recipes.tsx
@@ -1,6 +1,30 @@
+import useFetch from "../hooks";
+import { Recipe } from "../utils/types";
+
 function Recipes() {
+  const {
+    data: recipes,
+    error,
+    isLoading,
+  } = useFetch<Recipe[]>("/api/recipes");
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  console.log(recipes);
+  
   return (
-    <div>Recipes</div>
-  )
+    <div>
+      {recipes &&
+        recipes.map((recipe) => {
+          return <p key={recipe.id}>{recipe.name}</p>;
+        })}
+    </div>
+  );
 }
-export default Recipes
+export default Recipes;
