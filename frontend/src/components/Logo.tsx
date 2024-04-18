86e62ef8f8d106edc1065b9f529452e2ef8490cb
tolnabert <tolnabert@gmail.com>
2024-04-18 16:34:52 +0200

    add basic css for header and navbar

---
+++ frontend/src/components/Logo.tsx
@@ -0,0 +1,8 @@
+import "../css/logo.css"
+
+function Logo() {
+  return (
+    <div className="logo">Logo</div>
+  )
+}
+export default Logo
--- frontend/src/utils/links.ts
+++ frontend/src/utils/links.ts
@@ -4,11 +4,11 @@ type Link = {
 };
 
 export const links: Link[] = [
-  { href: "/", label: "home" },
-  { href: "/about-us", label: "about us" },
-  { href: "/recipes", label: "recipes" },
-  { href: "/favorite-recipes", label: "favorites" },
-  { href: "/my-recipes", label: "my recipes" },
-  { href: "/create-recipe", label: "create recipe" },
-  { href: "/shopping-list", label: "shopping list" },
+  { href: "/", label: "Home" },
+  { href: "/about-us", label: "About Us" },
+  { href: "/recipes", label: "Recipes" },
+  { href: "/favorite-recipes", label: "Favorites" },
+  { href: "/my-recipes", label: "My Recipes" },
+  { href: "/create-recipe", label: "Create Recipe" },
+  { href: "/shopping-list", label: "Shopping List" },
 ];
