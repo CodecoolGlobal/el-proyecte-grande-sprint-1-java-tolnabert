2f2deb8c746cbfca33aee763ae03a6842ae4e954
Tumpeti <118043221+Tumpeti@users.noreply.github.com>
2024-04-18 23:55:42 +0200

    add recipe sorting and ordering

--- frontend/src/pages/Recipes.tsx
+++ frontend/src/pages/Recipes.tsx
@@ -1,14 +1,32 @@
+import React, { useState } from "react";
+import RadioButton from "../components/RadioButton";
 import RecipeCard from "../components/RecipeCard";
 import useFetch from "../hooks";
 import { Recipe } from "../utils/types";
 import "./Recipes.css"
+import "./SortingOptions.css"
 
 function Recipes() {
+
+  const [selected, setSelected] = useState({
+    sortBy: "name",
+    sortOrder: "desc",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelected({
+      ...selected,
+      [e.target.name]: e.target.value,
+    });
+    
+  };
+  
+
   const {
     data: recipes,
     error,
     isLoading,
-  } = useFetch<Recipe[]>("/api/recipes");
+  } = useFetch<Recipe[]>( `/api/recipes?sortBy=${selected.sortBy}&sortOrder=${selected.sortOrder}`);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -21,10 +39,49 @@ function Recipes() {
   console.log(recipes);
 
   return (
+   
     <div>
-        <p>filter & sort & pagination</p>
-      <div className="recipe-container">
-        {/* to do on backend  */}
+    <div className="sorting-container">
+      <div className="sorting-box">
+        <h2>Order By:</h2>
+        <div className="sorting-options">
+          <RadioButton
+            name="sortBy"
+            labelText="Name"
+            value="name"
+            onChange={handleChange}
+            checked={selected.sortBy === "name"}
+          />
+          <RadioButton
+            name="sortBy"
+            labelText="Creation Date"
+            value="createdAt"
+            onChange={handleChange}
+            checked={selected.sortBy === "createdAt"}
+          />
+        </div>
+      </div>
+      <div className="sorting-box">
+        <h2>Sort By:</h2>
+        <div className="sorting-options">
+          <RadioButton
+            name="sortOrder"
+            labelText="Ascending"
+            value="asc"
+            onChange={handleChange}
+            checked={selected.sortOrder === "asc"}
+          />
+          <RadioButton
+            name="sortOrder"
+            labelText="Descending"
+            value="desc"
+            onChange={handleChange}
+            checked={selected.sortOrder === "desc"}
+          />
+        </div>
+      </div>
+    </div>
+   <div className="recipe-container">
         {recipes &&
           recipes.map((recipe) => (
             <RecipeCard key={recipe.id} recipe={recipe} />
