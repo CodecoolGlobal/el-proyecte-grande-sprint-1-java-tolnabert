deb52333ce364655d86e53979ea2bdf46ea8868f
Blanka Pasztor <139960849+blankapsz@users.noreply.github.com>
2024-04-18 20:15:58 +0200

    add more recipes page css

---
+++ frontend/src/components/RecipeCard.tsx
@@ -0,0 +1,20 @@
+import { Recipe } from "../utils/types";
+import "./css/RecipeCard.css"
+
+interface Props {
+    recipe: Recipe;
+}
+
+const RecipeCard = ({recipe}: Props) => {
+    return (
+        <div className="recipe-card">
+            <img src={recipe.image} />
+            <div className="recipe-card-title">
+                <h2>{recipe.name}</h2>
+                <h3>Description: {recipe.description}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default RecipeCard;
