ad18fe8485280eeca82ebbf2636b76ffd622e762
Tumpeti <118043221+Tumpeti@users.noreply.github.com>
2024-05-31 14:57:01 +0200

    removed redundant useSimpleFetch component

--- frontend/src/pages/Recipes.tsx
+++ frontend/src/pages/Recipes.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import RadioButton from "../components/RadioButton";
 import RecipeCard from "../components/RecipeCard";
-import useSimpleFetch from "../useSimpleFetch";
+import useFetch from "../hooks";
 import {Recipe} from "../utils/types";
 import "./Recipes.css"
 import "./SortingOptions.css"
@@ -25,7 +25,7 @@ function Recipes() {
         data: recipes,
         error,
         isLoading,
-    } = useSimpleFetch<Recipe[]>(`/api/recipes?sortBy=${selected.sortBy}&sortOrder=${selected.sortOrder}`, localStorage.getItem("jwtToken"));
+    } = useFetch<Recipe[]>(`/api/recipes?sortBy=${selected.sortBy}&sortOrder=${selected.sortOrder}`, localStorage.getItem("jwtToken"));
 
     if (isLoading) {
         return <div>Loading...</div>;
